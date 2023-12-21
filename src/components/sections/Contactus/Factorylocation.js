4dd7182c5c944b23eaced65fcf1d538d153671c7
website-krysta <website.krysta@gmail.com>
2023-12-21 18:53:42 +0530

    fixeed contact page

--- src/components/sections/Contactus/Factorylocation.js
+++ src/components/sections/Contactus/Factorylocation.js
@@ -4,7 +4,7 @@ class Factorylocation extends Component {
     render() {
         return (
             <div className="section" style={{ backgroundColor: '#f2f2f2' }}>
-                <div className="container">
+                <div className="container-fluid">
                     <div className="section-title-wrap text-center">
                         <h5 className="custom-primary  white-bg mx-auto">Explore Our Factory Locations</h5>
                         <h2 className="title">Where Innovation Takes Shape</h2>
@@ -23,9 +23,21 @@ class Factorylocation extends Component {
                                     <p className="card-text "  style={{ wordWrap: 'break-word' }} > P.No.37,47,48,49&49A, AutoNagar,Somasundarapalem,
                                         Kattevaram(Post),522295,
                                         TENALI,Guntur, A.P</p>
-                                    <button type="button" className=" btn-sm mail-btn mt-4">
+                                    <div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-phone' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="tel:9666389389" target='_blank'>9666389389</a></div>
+                                        </div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="mailto:madindradallmill9@gmail.com" target='_blank'>madindradallmill9@gmail.com</a></div>
+                                        </div>
+                                    </div>
+                                    {/* <button type="button" className=" btn-sm mail-btn mt-4">
                                         <a href="mailto: info@tenalidoublehorse.com" target='_blank'>Write Us <i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></a>
-                                    </button>
+                                    </button> */}
                                        
                                     
                                 </div>
@@ -39,12 +51,24 @@ class Factorylocation extends Component {
                                     <h5 className="card-title">MAHARANI DALL MILL</h5>
                                     <p className="card-text d-flex content-justify"> Plot.No.38,39,40,AutoNagar, Somasundarapalem,
                                         Kattevaram(Post)–522295, TENALI, Guntur Dist, A.P</p>
+                                        <div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-phone' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="tel:+91 85019 69798" target='_blank'>+91 85019 69798</a></div>
+                                        </div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="mailto: maharanidallmill@gmail.com" target='_blank'>maharanidallmill@gmail.com</a></div>
+                                        </div>
+                                    </div>
                                         {/* <p className="card-text content-justify d-flex justify-content-center p-text" >
                                     &nbsp;<a href = "mailto: maharanidallmill@gmail.com">Write us <i class='fas fa-arrow-right'  style={{ fontSize: '20px', color: '#7e8083' }}/></a>
                                     </p> */}
-                                    <button type="button" className=" btn-sm mail-btn mt-4">
+                                    {/* <button type="button" className=" btn-sm mail-btn mt-4">
                                         <a  href="mailto: maharanidallmill@gmail.com" target='_blank'>Write Us <i class=' pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></a>
-                                    </button>
+                                    </button> */}
                                 </div>
                             </div>
                         </div>
@@ -61,9 +85,21 @@ class Factorylocation extends Component {
                                     &nbsp;<a href = "mailto: mahadevadallindustries@gmail.com"><i class='fas fa-arrow-right'  style={{ fontSize: '20px', color: '#7e8083' }}/></a>
                                     </p>
                                      */}
-                                     <button type="button" className=" btn-sm mail-btn mt-4">
+                                      <div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-phone' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="tel:+91 91333 45456" target='_blank'>+91 91333 45456</a></div>
+                                        </div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="mailto: mahadevadallindustries@gmail.com" target='_blank'>mahadevadallindustries@gmail</a></div>
+                                        </div>
+                                    </div>
+                                     {/* <button type="button" className=" btn-sm mail-btn mt-4">
                                         <a   href="mailto: mahadevadallindustries@gmail.com" target='_blank'>Write Us <i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></a>
-                                    </button>
+                                    </button> */}
                                 </div>
                             </div>
                         </div>
@@ -77,9 +113,21 @@ class Factorylocation extends Component {
                                     {/* <p className="card-text content-justify d-flex justify-content-center p-text" >
                                     &nbsp;<a href = "mailto: tenalidoublehorsefoods@gmail.com"><i class='fas fa-arrow-right'  style={{ fontSize: '20px', color: '#7e8083' }}/></a>
                                     </p> */}
-                                    <button type="button" className=" btn-sm mail-btn">
+                                     <div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-phone' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="tel:8187896075" target='_blank'>8187896075</a></div>
+                                        </div>
+                                        <div  className='d-flex'>
+                                            <div><i class='pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></div>&nbsp;&nbsp;
+                                            &nbsp;
+                                            <div><a href="mailto: tenalidoublehorsefoods@gmail.com" target='_blank'>tenalidoublehorsefoods</a></div>
+                                        </div>
+                                    </div>
+                                    {/* <button type="button" className=" btn-sm mail-btn">
                                         <a href="mailto: tenalidoublehorsefoods@gmail.com" target='_blank'>Write Us <i class=' pl-2 fas fa-envelope' style={{ fontSize: '15px', color: '#7e8083' }} /></a>
-                                    </button>
+                                    </button> */}
                                 </div>
                             </div>
                         </div>
