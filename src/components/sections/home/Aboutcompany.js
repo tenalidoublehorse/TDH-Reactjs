98262ed14d12a33e1b714470114eb41c41ea6a29
website-krysta <website.krysta@gmail.com>
2024-01-02 14:08:36 +0530

    change about section

--- src/components/sections/home/Aboutcompany.js
+++ src/components/sections/home/Aboutcompany.js
@@ -30,23 +30,25 @@ class TimelineSection extends Component {
           
         return (
           <section id="timeline " style={{ ...backgroundStyles }}>
-              <div className="section-title-wrap section-header text-center pt-1">
+              {/* <div className="section-title-wrap section-header text-center pt-1">
                         <h2 className="title">Tenali Double Horse</h2>
                         <p className='largefont'>At Tenali Double Horse, our journey began in 2005 with a vision to bring excellence and trust to every household in India. Guided by our commitment to quality and customer satisfaction, we have since become a trusted name that resonates with millions.</p>
-              </div>
+              </div> */}
             <div className="container-fluid video-conatiner mt-3">
               <div className="row ourstory-section text-center"  >
-                <div className='col-lg-12   about-video'>
-
-                  {/* <ReactPlayer
-                    url={"./assets/video/tdh-chairmen-video.mp4"}
-                    controls
-                    poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}
-                    width="100%"
-                    height="60%"
-                  /> */}
-
-                  <div>
+                  <div className='col-sm-12 col-lg-6'>
+                    <div className="section-title-wrap section-header text-center pt-1">
+                      <h2 className="title">Tenali Double Horse</h2>
+                      <p className='largefont'>At Tenali Double Horse, our journey began in 2005 with a vision to bring excellence and trust to every household in India. Guided by our commitment to quality and customer satisfaction, we have since become a trusted name that resonates with millions.</p>
+                      {this.props.pagename.pagename === "AboutUs" ? (
+                    "") : (
+                    <Link to="/about" className="btn-custom btn-sm shadow-none suma mb-5">
+                      READ OUR STORY
+                    </Link>
+                  )}
+                    </div>
+                  </div>
+                  <div className='col-sm-12 col-lg-6   about-video'>
                   {this.props.pagename.pagename === "AboutUs" ? (
                     <iframe  src="https://www.youtube.com/embed/4haxVA5cTuY?si=A5FrVYfc6VYLRg4S" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='mb-5 mx-auto border5'  allowfullscreen></iframe> 
                     ) : (
@@ -56,14 +58,9 @@ class TimelineSection extends Component {
                  </div>
                  
                 
-                  {this.props.pagename.pagename === "AboutUs" ? (
-                    "") : (
-                    <Link to="/about" className="btn-custom btn-sm shadow-none suma mb-5">
-                      READ OUR STORY
-                    </Link>
-                  )}
+                 
                   {/* <Link to="/about" className=" btn-custom btn-sm shadow-none suma mb-5 "> READ OUR STORY </Link> */}
-                </div>
+                
               </div>
             </div>
           </section>
