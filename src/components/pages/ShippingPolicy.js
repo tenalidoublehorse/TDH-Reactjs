a11ee1824be705d60ea67e3c6230c875d557e186
website-krysta <website.krysta@gmail.com>
2024-01-06 11:28:56 +0530

    fixed top header and iamges

--- src/components/pages/ShippingPolicy.js
+++ src/components/pages/ShippingPolicy.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import MetaTags from "react-meta-tags";
-import Header from '../layouts/Headerinner';
+import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import Content from '../sections/shippingpolicy/Content';
 import Breadcrumbs from '../layouts/Breadcrumb';
