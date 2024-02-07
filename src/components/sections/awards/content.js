a6afa8e6fcff8215624ebe9f5c06e33d15cab7e1
somesetty manideep <developer@dt7solutions.com>
2024-02-07 11:51:03 +0530

    updatesgg

--- src/components/sections/awards/content.js
+++ src/components/sections/awards/content.js
@@ -5,7 +5,9 @@ import ReactPaginate from 'react-paginate';
 import { Card, Button, Modal } from 'react-bootstrap';
 import Feedback from '../home/Feedback';
 import { useHistory } from 'react-router-dom';
+
 import Messenger from '../home/Messenger';
+
 function Awards() {
 
   const [selectedCardData, setSelectedCardData] = useState(null);
@@ -27,6 +29,7 @@ function Awards() {
     // setShowModal(true);
     history.push(`/awardetails/${item.id}`);
   };
+  
 
   const startIndex = pageNumber * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -34,79 +37,99 @@ function Awards() {
   const displayedItems = instaData.slice(startIndex, endIndex);
 
   return (
-    <div className="ltn__gallery-area mt-5 mb-5">
-      <div className="section-title-wrap section-header text-center">
+    // <div className="ltn__gallery-area mt-5 mb-5">
+    //   <div className="section-title-wrap section-header text-center">
     
-        <h2 className="title text-center">Honors and Accolades That Define Us</h2>
-      </div>
-      <div className="container">
-      <div className='row awards '>
+    //     <h2 className="title text-center">Honors and Accolades That Define Us</h2>
+    //   </div>
+    //   <div className="container">
+    //   <div className='row awards '>
       
-            {displayedItems.map(item => (
-                // <div className='col-12 col-md-6 col-lg-6 mb-5'>
-                // <Card key={item.id} className='shadow'>
-                // <img src={process.env.PUBLIC_URL + "/" + item.imageUrl} alt="img" />
-                // <Card.Body >
-                //     <div className='d-flex justify-content-between align-items-center'>
-                //     <Card.Title className='c-title mb-0'>{item.year}</Card.Title>
-                //     <Button className='btn-md more-btn'  onClick={() => handleShow(item)}>
-                //         View
-                //     </Button>
-                    
-                //     </div>
-                //     <Card.Text className='c-title mt-2' >
-                //     {item.title}
-                //     </Card.Text>
-                    
+    //         {displayedItems.map(item => (
                
-                // </Card.Body>
-                // </Card>
-                // </div>
-                <Card  className=' d-flex flex-row flex-wrap mb-5 template-shadow rounded-3 wow animate__animated animate__fadeInUp' data-wow-delay={item.delay}>
-                <div  key={item.id} className='col-12 col-md-6 col-lg-6 py-3  '>
+    //             <Card  className=' d-flex flex-row flex-wrap mb-5 template-shadow rounded-3 wow animate__animated animate__fadeInUp' data-wow-delay={item.delay}>
+    //             <div  key={item.id} className='col-12 col-md-6 col-lg-6 py-3' >
                 
-                <img src={process.env.PUBLIC_URL + "/" + item.imageUrl} className='article-img' alt="img" />
+    //             <img src={process.env.PUBLIC_URL + "/" + item.imageUrl} className='article-img' alt="img" />
                 
                 
-                </div>
-                <div className='col-12 col-md-6 col-lg-6 d-flex justify-content-center align-items-center '>
-                <Card.Body className='spaces' >
+    //             </div>
+    //             <div className='col-12 col-md-6 col-lg-6 d-flex justify-content-center align-items-center '>
+    //             <Card.Body className='spaces' >
                     
-                    <Card.Title className='c-title font-bold mb-2 custom-primary'>{item.year}</Card.Title>
+    //                 <Card.Title className='c-title font-bold mb-2 custom-primary'>{item.year}</Card.Title>
                     
                     
                  
-                    <Card.Text className='c-title font-bold-two mt-2 mb-4' >
-                    {item.title}
-                    </Card.Text>
-                    <Button className='order-item btn-custom btn-sm shadow-none 
-                     '  onClick={() => handleShow(item)} style={{maxWidth:'fit-content'}}>
-                        View More<i className=" fas fa-arrow-right ml-2  mt-1"></i>
-                    </Button>
+    //                 <Card.Text className='c-title font-bold-two mt-2 mb-4' >
+    //                 {item.title}
+    //                 </Card.Text>
+    //                 <Button className='order-item btn-custom btn-sm shadow-none 
+    //                  '  onClick={() => handleShow(item)} style={{maxWidth:'fit-content'}}>
+    //                     View More<i className=" fas fa-arrow-right ml-2  mt-1"></i>
+    //                 </Button>
                
-                </Card.Body>
+    //             </Card.Body>
 
-                </div>
-                </Card>
+    //             </div>
+    //             </Card>
                 
-            ))}
+    //         ))}
        
           
-    </div>
+    // </div>
  
-        <ReactPaginate
-          previousLabel={'Previous'}
-          nextLabel={'Next'}
-          pageCount={pageCount}
-          onPageChange={handlePageChange}
-          containerClassName={'pagination'}
-          activeClassName={'active'}
-        />
-        <Feedback/>
-    
-      </div>
+    //     <ReactPaginate
+    //       previousLabel={'Previous'}
+    //       nextLabel={'Next'}
+    //       pageCount={pageCount}
+    //       onPageChange={handlePageChange}
+    //       containerClassName={'pagination'}
+    //       activeClassName={'active'}
+    //     />
+    //     <Feedback/>
+ 
+    //   </div>
+    // </div>
+    <div className="ltn__gallery-area pt-5 pb-5 awards">
+    <div className="section-title-wrap section-header text-center">
+      <h2 className="title text-center">Honors and Accolades That Define Us</h2>
     </div>
-    
+    <div className="container">
+      <div className='row '>
+        {displayedItems.map((item, index) => (
+          <div key={item.id} className='col-lg-12 '>
+            <Card style={{backgroundColor:'#fff7ef'}} className={`d-flex flex-row flex-wrap pb-5 border-middle  wow animate__animated animate__fadeInUp ${index % 2 === 0 ? 'even' : 'odd'}`} data-wow-delay={item.delay}>
+              <div className={`col-lg-6 bubble ${index % 2 === 0 ? 'order-lg-1 order  ' : 'order-lg-2 ' }`}>
+                <img src={process.env.PUBLIC_URL + "/" + item.imageUrl} className=' img-padding article-img  template-shadow rounded-3 ' alt="img" />
+              </div>
+              <div className={`col-lg-6 d-flex justify-content-center align-items-start ${index % 2 === 0 ? 'order-lg-2 ' : 'order-lg-1 '}`}>
+                <Card.Body className='spaces'>
+                  <Card.Title className='c-title font-bold mb-2 custom-primary'>{item.year}</Card.Title>
+                  <Card.Text className='c-title font-bold-two mt-2 mb-4'>{item.title}</Card.Text>
+                  <Button className='order-item btn-custom btn-sm shadow-none' onClick={() => handleShow(item)} style={{ maxWidth: 'fit-content' }}>
+                    View More<i className="fas fa-arrow-right ml-2 mt-1"></i>
+                  </Button>
+                </Card.Body>
+              </div>
+              { <div className="vertical-border"></div>}
+            </Card>
+          </div>
+        ))}
+      </div>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={handlePageChange}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
+      <Feedback/>
+     
+    </div>
+  </div>
+   
   );
 }
 
