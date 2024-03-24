ce88dbf439e7108d24af113746dca52d33fcec92
website-krysta <website.krysta@gmail.com>
2024-03-24 18:41:59 +0530

    fixed model issues

--- src/components/sections/home-v3/Lazyload.js
+++ src/components/sections/home-v3/Lazyload.js
@@ -3,32 +3,29 @@ import Button from 'react-bootstrap/Button';
 import { Modal } from 'react-bootstrap';
 
 function Popup() {
-  const [showModal, setShowModal] = useState(false);
-
-  useEffect(() => {
-    // Set showModal to true when the component mounts (page loads)
-    setShowModal(true);
-  }, []);
-  
+  const [showModal, setShowModal] = useState(true);
 
   const handleClose = () => {
-    // Close the modal
     setShowModal(false);
+  };
+
+  const handleToggle = () => {
+    setShowModal(!showModal);
   };
 
   return (
     <>
-    <Modal show={showModal} onHide={handleClose} centered className='popkl'>
-  <div style={{ position: 'relative' }}>
-    <Button variant="danger" onClick={handleClose} className='closebutton' style={{ position: 'absolute', top: '0', right: '0', zIndex: '999' }}>
-      <i className="fas fa-times" />
-    </Button>
-    <a href='https://ipltenalidoublehorse.com/' target='_blank'>
-      <img className='relative' src={process.env.PUBLIC_URL + "/assets/img/popup-banner-img.jpg"} alt="Gulfood Expo" />
-      <Button variant="primary" className="participate-button" style={{ position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)' }}>Participate</Button>
-    </a>
-  </div>
-</Modal>
+      <Modal show={showModal} onHide={handleClose} centered className='popkl'>
+        <div style={{ position: 'relative' }}>
+          <Button  onClick={handleClose} className='closebutton model-close-button' style={{ position: 'absolute', top: '0', right: '0', zIndex: '999' }}>
+            <i className="fas fa-times" />
+          </Button>
+          <a href='https://ipltenalidoublehorse.com/' target='_blank'>
+            <img className='relative' src={process.env.PUBLIC_URL + "/assets/img/popup-banner-img.jpg"} alt="Gulfood Expo" />
+          </a>
+          <Button variant="primary" className="participate-button mb-3" style={{ position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)' }}>Click Here</Button>
+        </div>
+      </Modal>
     </>
   );
 }
